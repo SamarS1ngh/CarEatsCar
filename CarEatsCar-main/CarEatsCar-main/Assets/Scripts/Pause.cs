f53754d0a79900a57ade5f19ff73282e25f67bf9
adviti1 <advitigangwar123@gmail.com>
2023-06-15 12:23:16 +0530

    pause button added

--- CarEatsCar-main/CarEatsCar-main/Assets/Scripts/Pause.cs
+++ CarEatsCar-main/CarEatsCar-main/Assets/Scripts/Pause.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
+    public Button pauseButton;
+
     private bool isPaused = false;
 
-   
+    private void Start()
+    {
+        // Attach the OnClick event handler to the button
+        pauseButton.onClick.AddListener(TogglePause);
+    }
+
+    private void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
     private void PauseGame()
     {
         Time.timeScale = 0f;
@@ -14,5 +34,11 @@ public class Pause : MonoBehaviour
         Debug.Log("Game Paused");
     }
 
+    private void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        Debug.Log("Game Resumed");
+    }
 
 }
